d3fadfcdb22391eaa4f0ee7bcb03923017e19b39
Tyti Nguyen <32077632+tytinguyen@users.noreply.github.com>
2018-11-14 19:44:59 -0500

    Add files via upload
    made cell smaller and generation iterate faster.
    start, stop, reset, +1 generation, +23 generation added and fully functional.

--- Gof/sketch.js
+++ Gof/sketch.js
@@ -17,68 +17,123 @@ while(d2-d < ms);
 let grid;
 let cols;
 let rows;
-let resolution = 50; //Edit for size of cell
+let generation = 0;
+let delay = true;
+let resolution = 10; //Edit for size of cell
 
 function setup() {
-  createCanvas(600, 600);
-  cols = width / resolution;
-  rows = height / resolution;
+  createCanvas(700, 700);
 
-  grid = make2DArray(cols, rows);
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[i][j] = floor(random(2));
+  buttonStart = createButton('Start');
+  buttonStart.mousePressed(loop);
+
+  buttonStop = createButton('Stop');
+  buttonStop.mousePressed(noLoop);
+
+    // buttonResume = createButton('Resume');
+    // buttonResume.mousePressed(loop);
+
+  buttonReset = createButton('Reset');
+    buttonReset.mousePressed(resetSketch);
+
+  buttonOneGeneration = createButton('+1 Generation');
+    buttonOneGeneration.mousePressed(redraw);
+
+  button23Generation = createButton('+23 Generations');
+    button23Generation.mousePressed(iterateGen);
+
+    resetSketch();
+
+  // cols = width / resolution;
+  // rows = height / resolution;
+
+  // grid = make2DArray(cols, rows);
+  // for (let i = 0; i < cols; i++) {
+  //   for (let j = 0; j < rows; j++) {
+  //     grid[i][j] = floor(random(2));
+  //   }
+  // }
+}
+function resetSketch(){
+  loop();
+    generation = 0;
+    cols = width / resolution;
+    rows = height / resolution;
+
+    grid = make2DArray(cols, rows);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
+        }
     }
-  }
+    noLoop();
 }
 
 //Draw function swtich Between old 2D to New 2D and rewite old 2D
 function draw() {
-  background(0);
+  // print("test 1");
 
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let x = i * resolution;
-      let y = j * resolution;
-      if (grid[i][j] == 1) {
-        fill(255);
-        stroke(0);
-        rect(x, y, resolution - 1, resolution - 1);
-      }
-    }
-    wait(500);  //Edit for time of state change in ms
-  }
+    if(delay == true) {
+      background(0);
 
-  let next = make2DArray(cols, rows);
-
-  // Compute next based on grid
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let myCell = grid[i][j];
-      // Count live neighbors!
-      let sum = 0;
-      let neighbors = countNeighbors(grid, i, j);
-      if (myCell == 0 && neighbors == 3) {         
-        next[i][j] = 1;  //a dead cell with exactly three live neighbors becomes alive in all other cases a dead cell will stay dead.
-      }else if (myCell == 1 && (neighbors == 0 || neighbors == 1)) { //a live cell with zero or one live neighbors will die.
-        next[i][j] = 0;
-      }else if(myCell == 1 && (neighbors == 2 || neighbors == 3)){  //a live cell with two or three live neighbors will remain alive
-          next[i][j] = 1;
-      }else if(myCell == 1 && (neighbors >= 4)){  //a live cell with four or more live neighbors will die. 
-         next[i][j] = 0;
-      } else {
-        next[i][j] = myCell;
+      for (let i = 0; i < cols; i++) {
+          for (let j = 0; j < rows; j++) {
+              let x = i * resolution;
+              let y = j * resolution;
+              if (grid[i][j] == 1) {
+                  fill(255);
+                  stroke(0);
+                  rect(x, y, resolution - 1, resolution - 1);
+              }
+          }
+          wait(1);  //Edit for time of state change in ms
       }
 
-    }
-    
+      let next = make2DArray(cols, rows);
+
+      // Compute next based on grid
+      for (let i = 0; i < cols; i++) {
+          for (let j = 0; j < rows; j++) {
+              let myCell = grid[i][j];
+              // Count live neighbors!
+              let sum = 0;
+              let neighbors = countNeighbors(grid, i, j);
+              if (myCell == 0 && neighbors == 3) {
+                  next[i][j] = 1;  //a dead cell with exactly three live neighbors becomes alive in all other cases a dead cell will stay dead.
+              } else if (myCell == 1 && (neighbors == 0 || neighbors == 1)) { //a live cell with zero or one live neighbors will die.
+                  next[i][j] = 0;
+              } else if (myCell == 1 && (neighbors == 2 || neighbors == 3)) {  //a live cell with two or three live neighbors will remain alive
+                  next[i][j] = 1;
+              } else if (myCell == 1 && (neighbors >= 4)) {  //a live cell with four or more live neighbors will die.
+                  next[i][j] = 0;
+              } else {
+                  next[i][j] = myCell;
+              }
+
+          }
+          // print("this is " + generation + "generation");
+
+
+      }
+        document.getElementById("demo").innerHTML = "this is the " + generation + "th generation";
+        generation++;
+
+      grid = next;
   }
-
-  grid = next;
-
-
-
 }
+function pauseGame() {
+    delay = false;
+}
+function startGame() {
+    delay = true;
+}
+function iterateGen(){
+  for(let i = 0; i < 23; i++){
+    redraw();
+  }
+}
+
 
 // count number of neighbors for all cell 
 function countNeighbors(grid, x, y) {
